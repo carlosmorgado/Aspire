462a8274cf8f89f31464748b3a867d7f6ac47653
Carlos Morgado <5589959+carlosmorgado@users.noreply.github.com>
2024-08-14 18:01:18 +0100

    chore(initial-setup)

---
+++ src/CarlosMorgado.Aspire.MongoDB/AspireMongoDBDriverExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Microsoft.Extensions.Hosting;
+
+public static class AspireMongoDBDriverExtensions
+{
+    public static void AddMongoDbClient(
+        this IHostApplicationBuilder builder,
+        string connectionName)
+    {
+        ArgumentNullException.ThrowIfNull(builder, nameof(builder));
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(connectionName, nameof(connectionName));
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.AddMongoDBClient(connectionName);
+            return;
+        }
+
+        if (builder.Configuration.GetConnectionString(connectionName) is not string connectionString)
+        {
+            throw new InvalidOperationException("No connection string found for non Development run.");
+        }
+
+        var clientSettings = MongoClientSettings.FromConnectionString(connectionString);
+
+        builder.Services.AddSingleton<IMongoClient>(new MongoClient(clientSettings));
+    }
+}
